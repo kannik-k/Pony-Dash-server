227e434bd1880ceb3feae31c6e5fc051f5081969
aekald <aekald@taltech.ee>
2024-02-08 16:55:50 +0200

    server setup start

--- server/src/main/java/ee/taltech/game/server/GameServer.java
+++ server/src/main/java/ee/taltech/game/server/GameServer.java
@@ -1,5 +1,7 @@
 package ee.taltech.game.server;
 
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 import java.io.IOException;
@@ -16,6 +18,11 @@ public class GameServer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        server.addListener(new Listener() {
+            public void received (Connection connection, Object object) {
+                System.out.println(object);
+            }
+        });
     }
 
     public static void main(String[] args) {
