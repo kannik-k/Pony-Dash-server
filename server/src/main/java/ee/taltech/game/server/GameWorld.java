9db09905c131d3d8839eb3ce9b8f03eb3d19eae1
aekald <aekald@taltech.ee>
2024-04-08 14:53:53 +0300

    ai

--- server/src/main/java/ee/taltech/game/server/GameWorld.java
+++ server/src/main/java/ee/taltech/game/server/GameWorld.java
@@ -1,11 +1,24 @@
 package ee.taltech.game.server;
 
 import ee.taltech.game.server.ai.NPC;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.StringReader;
+import java.util.Objects;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
 public class GameWorld {
+    private int gameId;
+    int[][] collisions;
     private List<NPC> aiBots = new ArrayList<>();
 
     /**
@@ -14,6 +27,43 @@ public class GameWorld {
      */
     public GameWorld(int gameId) {
         this.generateAiBots();
+        this.readTilemapData();
+    }
+
+    private void readTilemapData () {
+        try {
+            File file = new File("Server/data/MapGeneral.tmx");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("layer");
+
+            // First collision layer
+            Node node = nodeList.item(6); // Layer 6
+            Element eElement = (Element) node;
+            String data = eElement.getElementsByTagName("data").item(0).getTextContent();
+            String[] columns = data.split("\n");
+            int[][] collisions = new int[columns.length][columns[1].split(",").length + 1];
+            int y = columns.length - 1;
+            int x = 0;
+            for (String column : columns) {
+                String[] rows = column.split(",");
+                x = 0;
+                for (String row : rows) {
+                    if (!Objects.equals(row, "") && Integer.parseInt(row) > 0) {
+                        collisions[y][x] = 1;
+                    }
+                    x++;
+                }
+                y--;
+            }
+            this.collisions = collisions;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     /**
