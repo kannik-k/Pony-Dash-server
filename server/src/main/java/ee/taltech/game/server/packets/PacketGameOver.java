368bf91fd281a9f77f65d3f09b53c83a9b70c1f0
Karoliina <kannik@taltech.ee>
2024-04-24 00:34:14 +0300

    lõputingimuste realiseerimine

---
+++ server/src/main/java/ee/taltech/game/server/packets/PacketGameOver.java
@@ -0,0 +1,22 @@
+package ee.taltech.game.server.packets;
+
+public class PacketGameOver {
+    private int playerId;
+    private int gameId;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+}
